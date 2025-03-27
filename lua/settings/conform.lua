3e4e8ff5fea708d6ece6f623bf9cfd9aa525e678
bella <maekawatakaya10.8@gmail.com>
2025-03-27 09:52:57 +0900

    add python formatter: ruff

--- lua/settings/conform.lua
+++ lua/settings/conform.lua
@@ -1,4 +1,7 @@
 require("conform").setup({
+  formatters_by_ft = {
+    python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+  },
   format_on_save = function(bufnr)
     local ignore_filetypes = { "ejs", "html" }
     if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
@@ -17,53 +20,3 @@ require("conform").setup({
     return { timeout_ms = 500, lsp_format = "fallback" }
   end,
 })
-
--- vim.api.nvim_create_autocmd("BufReadPost", {
---   pattern = "*",
---   callback = function()
---     require("conform").format({
---       lsp_fallback = true,
---       async = false,
---     })
---   end,
--- })
-
--- local excluded_filetypes = { "ejs" }
-
--- local function unpack(list)
---   local result = {}
---   for i, v in ipairs(list) do
---     result[v] = false
---   end
---   return result
--- end
-
--- require("conform").setup({
---   filetypes = unpack(excluded_filetypes),
---   formatters_by_ft = {
---     ejs = { "prettier" },
---   },
--- })
-
--- local format_if_not_excluded = function()
---   local current_filetype = vim.bo.filetype
---   for _, filetype in ipairs(excluded_filetypes) do
---     if current_filetype == filetype then
---       return
---     end
---   end
---   require("conform").format({
---     lsp_fallback = true,
---     async = false,
---   })
--- end
-
--- vim.api.nvim_create_autocmd("BufReadPost", {
---   pattern = "*",
---   callback = format_if_not_excluded,
--- })
-
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*",
---   callback = format_if_not_excluded,
--- })
