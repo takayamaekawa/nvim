fc896e85cc25221cb7e37c183920b78f143148b3
bella <maekawatakaya10.8@gmail.com>
2025-03-27 10:08:16 +0900

    add rust_analyzer

--- lua/settings/lsp/mylsp.lua
+++ lua/settings/lsp/mylsp.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "jdtls", "bashls", "clangd", "ts_ls", "jsonls", "pyright", "html", "dockerls", },
+  ensure_installed = { "lua_ls", "jdtls", "bashls", "clangd", "ts_ls", "jsonls", "pyright", "html", "dockerls", "rust_analyzer" },
   automatic_installation = true,
 })
 
