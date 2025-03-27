dc819f5c4b6074e2b776f7ddad1672a60d5dfeab
bella <maekawatakaya10.8@gmail.com>
2025-03-27 11:04:35 +0900

    add shfmt formatter

--- lua/settings/conform.lua
+++ lua/settings/conform.lua
@@ -1,6 +1,8 @@
 require("conform").setup({
   formatters_by_ft = {
     python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+    sh = { "shfmt" },
+    bash = { "shfmt" },
   },
   format_on_save = function(bufnr)
     local ignore_filetypes = { "ejs", "html" }
