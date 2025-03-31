73929b19c929ab6da0f938cc8ffbc5ff5107d728
bella <maekawatakaya10.8@gmail.com>
2025-03-31 20:48:33 +0900

    change keymap <leader>nv -> <leader>sr, source reload

--- lua/settings/keymaps/file_match.lua
+++ lua/settings/keymaps/file_match.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'lua',
   callback = function()
-    vim.keymap.set('n', '<leader>nv', function()
+    vim.keymap.set('n', '<leader>sr', function()
       vim.cmd('source %')
-      vim.notify("Current file is sourced.", "info")
+      vim.notify("Current file source is reloaded.", "info")
     end, { desc = 'Source current Lua file' })
   end,
 })
