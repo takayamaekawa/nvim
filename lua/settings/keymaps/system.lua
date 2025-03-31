0d75dce9c89fd4b252c90bba8c817432347117ae
bella <maekawatakaya10.8@gmail.com>
2025-03-31 19:20:26 +0900

    add function of being able to copy some kinds of files content and that of relative path at the same time

--- lua/settings/keymaps/system.lua
+++ lua/settings/keymaps/system.lua
@@ -45,6 +45,64 @@ fmap('n', '<Leader>fy', function()
   vim.notify("Copied the current file's relative path and content. (" .. current_time .. ")", "info")
 end, opts)
 
+-- clip the all files matched extension in that project directory
+fmap('n', '<Leader>fd', function()
+  local dir_paths = vim.fn.input("Enter the relative path. (e.g., /path/to/project,/path/to/project2): ",
+    ".", "file")
+  local extensions = vim.fn.input("Enter the included file extensions. (e.g., txt,py,java): ", "*", "file")
+  local exclude_paths = vim.fn.input("Enter the excluded directory path. (e.g., node_modules,venv): ", "", "file")
+
+  local dir_list = vim.split(dir_paths, ",")
+  local ext_list = vim.split(extensions, ",")
+  local exclude_list = vim.split(exclude_paths, ",")
+
+  local matched_files = {}
+
+  for _, dir_path in ipairs(dir_list) do
+    local abs_dir_path = vim.fn.expand(dir_path)
+    local files = vim.fn.readdir(abs_dir_path)
+
+    for _, file in ipairs(files) do
+      local file_path = vim.fn.join({ abs_dir_path, file }, "/")
+
+      local file_ext = string.match(file, "%.(%w+)$")
+      local is_excluded = false
+
+      if #exclude_list > 0 and exclude_paths ~= "" then
+        for _, exclude_path in ipairs(exclude_list) do
+          if vim.fn.match(file_path, exclude_path) ~= -1 then
+            is_excluded = true
+            break
+          end
+        end
+      end
+
+      if not is_excluded and file_ext ~= "" then
+        for _, ext in ipairs(ext_list) do
+          if file_ext == ext or extensions == "*" then
+            table.insert(matched_files, file_path)
+            break
+          end
+        end
+      end
+    end
+  end
+
+  if #matched_files == 0 then
+    vim.notify("Nothing matched file.", "warn")
+  else
+    local clipboard_content = ""
+    for _, file_path in ipairs(matched_files) do
+      local lines = vim.fn.readfile(file_path)
+      clipboard_content = clipboard_content ..
+          vim.fn.expand(file_path .. ':.:h') ..
+          '/' .. vim.fn.expand(file_path .. ':t') .. '\n' .. table.concat(lines, "\n") .. '\n'
+    end
+    vim.fn.setreg('+', clipboard_content)
+    vim.notify("Copied the all matched files' relative path and content. (" .. current_time .. ")", "info")
+  end
+end, opts)
+
 -- terminal
 -- open by default nvim's one
 map('n', '<leader>ts', '<Cmd>terminal<CR>', opts);
