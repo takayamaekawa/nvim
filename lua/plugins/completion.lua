339a0c01c39330ae6705375efc84264bfd677dea
bella <maekawatakaya10.8@gmail.com>
2025-05-10 16:05:41 +0900

    fix completion problem that suggests a lot words

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,8 +3,8 @@ return {
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-vsnip" },
-    { "hrsh7th/vim-vsnip" },
+    -- { "hrsh7th/cmp-vsnip" },
+    -- { "hrsh7th/vim-vsnip" }, -- 使ってない
     -- { "hrsh7th/cmp-buffer" },
     { "hrsh7th/cmp-path" },
     { "hrsh7th/cmp-cmdline" },
@@ -14,17 +14,17 @@ return {
     local conf = {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "vsnip" },
+        -- { name = "vsnip" },
         -- { name = "buffer" },
-        { name = "path" },
-        { name = "cmdline" },
+        -- { name = "path" },
+        -- { name = "cmdline" },
       }),
-      snippet = {
-        expand = function(args)
-          -- Comes from vsnip
-          vim.fn["vsnip#anonymous"](args.body)
-        end,
-      },
+      -- snippet = {
+      --   expand = function(args)
+      --     -- Comes from vsnip
+      --     vim.fn["vsnip#anonymous"](args.body)
+      --   end,
+      -- },
       mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -32,9 +32,9 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
       }),
-      completion = {
-        completeopt = 'menu,menuone,noselect',
-      },
+      -- completion = {
+      --   completeopt = 'menu,menuone,noselect',
+      -- },
     }
 
     cmp.setup.cmdline({ "/", "?" }, {
