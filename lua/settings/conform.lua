30b947c17627b29b767a16efcd3afe3f4e05cfd3
bella <maekawatakaya10.8@gmail.com>
2025-05-22 00:57:56 +0900

    add php formatter

--- lua/settings/conform.lua
+++ lua/settings/conform.lua
@@ -1,11 +1,26 @@
 require("conform").setup({
+  log_level = vim.log.levels.DEBUG,
+  formatters = {
+    php_cs_fixer = {
+      command = "/home/bella/git/bbs/vendor/bin/php-cs-fixer",
+      -- args = { "fix", "--config=.php-cs-fixer.dist.php", "--using-cache=no" },
+      args = { "fix", "$FILENAME", "--config=.php-cs-fixer.dist.php", "--using-cache=no" },
+    },
+  },
   formatters_by_ft = {
     python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
     sh = { "shfmt" },
     bash = { "shfmt" },
+    php = { "php_cs_fixer" },
   },
   format_on_save = function(bufnr)
     local ignore_filetypes = { "ejs", "html" }
+    local ignore_by_external_filetypes = { "php" }
+
+    if vim.tbl_contains(ignore_by_external_filetypes, vim.bo[bufnr].filetype) then
+      return { timeout_ms = 2000 } -- 例: 2秒 (2000ミリ秒) に増やす。必要に応じてさらに増やす。
+    end
+
     if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
       return
     end
@@ -19,6 +34,7 @@ require("conform").setup({
       return
     end
 
+    -- その他のファイルタイプはlsp_formatにフォールバック
     return { timeout_ms = 500, lsp_format = "fallback" }
   end,
 })
