a1c0171f43d67b8f6bcbaf4740cb2407902424f0
Takaya Maekawa <takaya@maekawa.dev>
2025-08-16 21:48:27 +0900

    feat: format all java file

--- lua/settings/commands/lsp.lua
+++ lua/settings/commands/lsp.lua
@@ -4,3 +4,35 @@ local diagnostic_utils = require("utils.diagnostic")
 
 userCommand('LspErrorsQuickfix', diagnostic_utils.ShowLspDiagnosticsInQuickfix, {})
 userCommand("LspErrorTelescope", diagnostic_utils.show_error_files, {})
+
+local function format_all_java_files()
+  local handle = io.popen("find . -name '*.java' -type f")
+  if not handle then
+    print("Failed to execute find command")
+    return
+  end
+
+  local java_files = {}
+  for file in handle:lines() do
+    table.insert(java_files, file)
+  end
+  handle:close()
+
+  if #java_files == 0 then
+    print("No Java files found in current directory")
+    return
+  end
+
+  print("Found " .. #java_files .. " Java files. Formatting...")
+
+  for _, file in ipairs(java_files) do
+    vim.cmd("edit " .. vim.fn.fnameescape(file))
+    vim.lsp.buf.format({ timeout_ms = 5000 })
+    vim.cmd("write")
+    print("Formatted: " .. file)
+  end
+
+  print("Completed formatting " .. #java_files .. " Java files")
+end
+
+userCommand('FormatAllJava', format_all_java_files, { desc = 'Format all Java files in current directory' })
